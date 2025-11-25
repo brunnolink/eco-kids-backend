23a0a1e7bcbcb8a874eed2007695d0a726675b86
brunno <brunnolinker@gmail.com>
2025-11-25 17:36:59 -0300

    chore: -cors allowed

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,PATCH,DELETE",
+}));
 app.use(express.json());
 app.use(appRouter);
 
