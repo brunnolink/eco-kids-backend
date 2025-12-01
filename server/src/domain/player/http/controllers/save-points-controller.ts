26220f8bae6a995d61826e332ee2165d4c275b3f
brunno <brunnolinker@gmail.com>
2025-12-01 15:44:57 -0300

    fix: -name of the variable 'achievements'

--- server/src/domain/player/http/controllers/save-points-controller.ts
+++ server/src/domain/player/http/controllers/save-points-controller.ts
@@ -2,10 +2,10 @@ import type { Request, Response } from 'express'
 import { makeSavePointsService } from '../../services/factories/make-create-player';
 export class SavePointsController {
     async create(request: Request, response: Response): Promise<Response> {
-        const { playerId, points, achievenments } = request.body;
+        const { playerId, points, achievements } = request.body;
         const service = makeSavePointsService();
         try {
-            const player = await service.savePlayerPoints(playerId, points, achievenments);
+            const player = await service.savePlayerPoints(playerId, points, achievements);
             return response.status(201).json(player);
         } catch (error) {
             if (error instanceof Error) {
